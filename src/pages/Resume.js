3d12773aa81f5b3753ec223e491104dc58bc7a50
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2022-01-15 10:56:56 -0500

    working button for download resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,18 +1,16 @@
 import React from "react";
+import resume from "../assets/images/az.jpg"
 
 const Resume = () => (
   <div>
     <h1>Resume Page</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-      varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-      Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-      imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum ligula
-      in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare elit.
-      Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-      consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-      fames ac ante ipsum primis in faucibus.
-    </p>
+    <button src="https://user-images.githubusercontent.com/85147307/149609284-3b9d1424-beb1-4677-904e-4886904f5b6e.jpg" download="az">
+    <a
+        href="https://user-images.githubusercontent.com/85147307/149609284-3b9d1424-beb1-4677-904e-4886904f5b6e.jpg"
+        download
+      >
+        Click to download
+      </a>  </button> 
   </div>
 );
 
